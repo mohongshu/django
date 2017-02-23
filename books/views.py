7ed8a916201caf1f27ef0dc9d1e4ec44a3a7f726
mohongshu <mo.hongshu@163.com>
2017-02-23 16:48:45 +0800

    chapter7 2017-2-23

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
+from books.models import Book
 # Create your views here.
 def display_meta(request):
     values = request.META.items()
@@ -19,3 +20,12 @@ def search(request):
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
+
+def search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render_to_response('search_results.html',
+            {'books': books, 'query': q})
+    else:
+        return HttpResponse('Please submit a search term.')
--- mysite/settings.py
+++ mysite/settings.py
@@ -64,12 +64,12 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'ENGINE': 'mysql.connector.django',  
+        #'ENGINE': 'mysql.connector.django',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'django',  
-        'USER':'root',  
+        'NAME':'django',
+        'USER':'root',
         'PASSWORD':'123456',  
-        'HOST':'127.0.0.1',  
+        'HOST':'127.0.0.1',
         'PORT':'3306',
     }
 }
