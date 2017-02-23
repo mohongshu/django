ed1545745f4c5272c41d7dab5697d819341143ef
mohongshu <mo.hongshu@163.com>
2017-02-23 17:59:05 +0800

    chapter7 2017-02-23 17:57

--- books/views.py
+++ books/views.py
@@ -14,18 +14,17 @@ def display_meta(request):
 def search_form(request):
     return render_to_response('search_form.html')
 
-def search(request):
-    if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    errors = []
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html',
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
             {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please submit a search term.')
+    return render_to_response('search_form.html', {'errors': errors})
